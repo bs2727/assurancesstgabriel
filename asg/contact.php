b8a5f4aa3f4ce6410f9413ea70d1e2a93e5d7081
schmitt.tho <schmitt.tho@stgabriel.auteuil.org>
2024-09-27 13:44:44 +0200

    adapter la typographie

--- asg/contact.php
+++ asg/contact.php
@@ -1,45 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Assurances Saint Gabriel</title>
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/styles.css" rel="stylesheet" />
-    </head>
+    <?php include_once("head.php"); ?>
     <body>
-        <header>
-            <h1 class="site-heading text-center text-faded d-none d-lg-block">
-               <span class="site-heading-upper text-primary mb-3">Assurances Saint Gabriel</span>
-                <span class="site-heading-lower">Nous sommes là pour vous, à vos côtés, dans les mauvais comme dans les bons moments de votre vie. </span>
-            </h1>
-        </header>
-        <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
-            <div class="container">
-                <a class="navbar-brand text-uppercase fw-bold d-lg-none" href="index.php">Start Bootstrap</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mx-auto">
-                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="index.php">Accueil</a></li>
-                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="about.php">Nos activités</a></li>
-                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="products.php">Nos contrats</a></li>
-                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="crew.php">Nos équipes</a></li>
-						<li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="contact.php">Nous contacter</a></li>
-
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <section class="page-section cta">
+		<?php include_once("header.php"); ?>
+        <?php include_once("nav.php"); ?>
+       <section class="page-section cta">
             <div class="container">
                 <div class="row">
                     <div class="col-xl-9 mx-auto">
@@ -118,12 +83,12 @@
                 </div>
             </div>
         </section>
-        <footer class="footer text-faded text-center py-5">
-            <div class="container"><p class="m-0 small">Copyright &copy; Assurances Saint Gabriel 2022</p></div>
-        </footer>
+
+        <?php include_once("footer.php"); ?>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
         <script src="js/scripts.js"></script>
     </body>
 </html>
+
