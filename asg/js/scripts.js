dcd81986b5718d755dc4e813ab8303a1cdefd5e8
bs2727 <mailbs92i@gmail.com>
2024-11-01 17:50:38 +0100

    fix font et erreur js

--- asg/js/scripts.js
+++ asg/js/scripts.js
@@ -5,6 +5,4 @@
 */
 // Highlights current date on contact page
 window.addEventListener('DOMContentLoaded', event => {
-    const listHoursArray = document.body.querySelectorAll('.list-hours li');
-    listHoursArray[new Date().getDay()].classList.add((''));
 })
