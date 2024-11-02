e54e2f8e722a12663d49c774342ada49f9f77ed3
bs2727 <mailbs92i@gmail.com>
2024-11-02 17:16:38 +0100

    include pour rediriger les utilisateurs

---
+++ asg/backend/redirect.php
@@ -0,0 +1,26 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <?php require_once "../includes/head.php"; ?>
+    <body>
+        <?php require_once "../includes/header.php"; ?>
+        <?php require_once "../includes/nav.php"; ?>
+        <script>
+        setTimeout(function() {
+        window.location.href = '../pages/formlogin.php';
+          }, 5000);
+        </script>
+        <div class="container my-5">
+            <div class="alert alert-warning text-center" role="alert">
+                <h2 class="section-heading mb-3">
+                    <span class="section-heading-upper">Vous n'êtes pas connecté</span>
+                    <br>
+                    <span class="section-heading-lower">Redirection vers la page de connexion...</span>
+                </h2>
+            </div>
+        </div>
+
+        <?php require_once "../includes/footer.php"; ?>
+    </body>
+    </html>
+    <?php
+    exit();
